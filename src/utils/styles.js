f902c2283e0b2b160caed7727d646f386959962b
Manuel Scholz <manuel.serrano943@gmail.com>
2025-06-20 19:16:44 +0200

    Feat: Fichero styles comunes

---
+++ src/utils/styles.js
@@ -0,0 +1,67 @@
+export const colors = {
+    primary: "#121212",
+    accent: "#FF7043",
+    text: "#f1f1f1"
+}
+
+export const ui = {
+    img: {
+        aspectRatio: 1,
+        width: 35,
+    },
+    text: {
+        fontFamily: "ancizar-regular",
+        color: colors.text,
+        fontSize: 17.5,
+        lineHeight: 17,
+    },
+    muted: {
+        fontFamily: "ancizar-regular",
+        color: colors.text,
+        fontSize: 14,
+    },
+    h1: {
+        fontSize: 33,
+        fontFamily: "ancizar-bold",
+        color: colors.text,
+        lineHeight: 32,
+    },
+    h2: {
+        fontFamily: "ancizar-bold",
+        color: colors.text,
+        fontSize: 28,
+        lineHeight: 28,
+    },
+    h3: {
+        fontFamily: "ancizar-bold",
+        color: colors.text,
+        fontSize: 22,
+        lineHeight: 24,
+    },
+    h4: {
+        fontFamily: "ancizar-bold",
+        color: colors.text,
+        fontSize: 20,
+    },
+    h5: {
+        fontFamily: "ancizar-bold",
+        color: colors.text,
+        fontSize: 17.5,
+    },
+    error: {
+        fontFamily: "ancizar-medium",
+        fontSize: 18,
+        color: "#ED4337",
+        textDecorationLine: "underline",
+        textDecorationStyle: "solid",
+        textDecorationColor: "#ED4337",
+        textAlign: "center"
+    },
+    subError: {
+        fontFamily: "ancizar-medium",
+        fontSize: 13,
+        color: "#ED4337",
+        textAlign: "center"
+    }
+
+}
